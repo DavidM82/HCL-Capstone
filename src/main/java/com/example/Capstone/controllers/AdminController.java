4a42436455a9ea25d91d8dbeff04826ad2fad544
David Mauriello <dmauriello82@gmail.com>
2021-04-08 11:17:46 -0400

    ProductInventory page can be called, product backend needs fixing

--- src/main/java/com/example/Capstone/controllers/AdminController.java
+++ src/main/java/com/example/Capstone/controllers/AdminController.java
@@ -4,20 +4,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-
-
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.Capstone.entities.Admin;
+import com.example.Capstone.entities.Product;
 import com.example.Capstone.services.AdminService;
+import com.example.Capstone.services.ProductService;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 
 @Controller
 public class AdminController {
 
 	@Autowired
 	AdminService adminService;
+	
+	@Autowired
+	ProductService productService;
 
 	@PostMapping(value="/admin/login")
 	public ResponseEntity<?> adminLogin(@RequestParam String username, @RequestParam String password){
@@ -37,5 +43,15 @@ public class AdminController {
 	@GetMapping("/admin")
 	public String showAdmin() {
 		return "adminHome";
+	}
+	
+	@RequestMapping(value = "/adminInventory", method = RequestMethod.GET)
+	public String adminProducts(Model model) {
+		
+		Iterable<Product> product = productService.findAllProduct();
+		
+		model.addAttribute("product", product);
+		
+		return "adminInventory";
 	}
 }
